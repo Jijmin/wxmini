edf27b2ae21f9b097c8f2457e4c82240e929b8fb
Jijmin <503004784@qq.com>
2018-02-07 17:01:34 +0800

    将文章数据填充到详情页

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -1,3 +1,13 @@
+var postsData = require('../../../data/posts-data.js');
 Page({
+  data: {
 
+  },
+  onLoad: function(option){
+    var postId = option.id;
+    var postData = postsData.postList[postId];
+    this.setData({
+      postData: postData
+    })
+  }
 })
