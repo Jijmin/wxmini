c68cad0361d92523f04056fb435ae5227df47366
Jijmin <503004784@qq.com>
2018-02-07 17:28:25 +0800

    点击文章主图播放音乐，显示音乐封面图

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -1,13 +1,32 @@
 var postsData = require('../../../data/posts-data.js');
 Page({
   data: {
-
+    currentPostId: null,
+    isPlayingMusic: false
   },
-  onLoad: function(option){
+  onLoad: function (option) {
     var postId = option.id;
+    this.data.currentPostId = postId;
     var postData = postsData.postList[postId];
     this.setData({
       postData: postData
     })
+  },
+  onMusicTap: function (event) {// 点击音乐播放器
+    var currentPostId = this.data.currentPostId;
+    var postData = postsData.postList[currentPostId];
+    var isPlayingMusic = this.data.isPlayingMusic;
+    if (isPlayingMusic) {// 暂停音乐
+
+    } else {// 播放音乐
+      wx.playBackgroundAudio({
+        dataUrl: postData.music.url,
+        title: postData.music.title,
+        coverImgUrl: postData.music.coverImg
+      });
+      this.setData({
+        isPlayingMusic: true
+      });
+    }
   }
 })
