5387a17d8c524e32308f49ac81aeaa3db08abc42
Jijmin <503004784@qq.com>
2018-02-08 13:07:05 +0800

    配置appid，豆瓣的API请求地址改了

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -1,66 +1,27 @@
-// pages/movies/movies.js
+var app = getApp();
 Page({
+  data: {},
+  onLoad: function(){
+    var inTheatersUrl = app.globalData.doubanBase + '/v2/movie/in_theaters?start=0&count=3';// 正在热映
+    var comingSoonUrl = app.globalData.doubanBase + '/v2/movie/coming_soon?start=0&count=3';// 即将上映
+    var top250Url = app.globalData.doubanBase + '/v2/movie/top250?start=0&count=3';// Top250
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-  
+    this.getMovieListData(inTheatersUrl);
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  getMovieListData: function(url){// 获取电影数据
+    var that = this;
+    wx.request({
+      url: url,
+      method: 'GET',
+      header: {
+        "Content-Type": "json"
+      },
+      success: function(res){
+        console.log(res);
+      },
+      fail: function (error) {
+        console.log(error)
+      }
+    })
   }
 })
