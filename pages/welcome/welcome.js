7cf99394ead945602181ebe612a670534ab3ac2b
Jijmin <503004784@qq.com>
2018-02-09 09:21:55 +0800

    欢迎页面获取用户登录态

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,7 +1,25 @@
 Page({
+  data: {
+    nickName:'Jijmin',
+    avatarUrl: '/images/avatar/1.png'
+  },
   onTap: function(event){
     wx.switchTab({
       url: '../posts/post',
     });
+  },
+  onLoad: function(){
+    var that = this;
+    wx.getUserInfo({
+      success: function (res) {
+        var userInfo = res.userInfo;
+        var nickName = userInfo.nickName;
+        var avatarUrl = userInfo.avatarUrl;
+        that.setData({
+          nickName: nickName,
+          avatarUrl: avatarUrl
+        });
+      }
+    })
   }
 })
